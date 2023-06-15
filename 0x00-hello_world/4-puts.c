98630fbbdf87173f41616c3e285dee010bbf2adf
salah <salahedinelassri@gmail.com>
2023-06-15 12:23:18 -0700

    OUF

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
- *main - Rntry point
+ * main - Entry point
  *
  * Return: always 0 (Success)
  *
 */
 
-
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
