f931dbfd364374f164d16f8c5c9f5ff739b77768
salah <salahedinelassri@gmail.com>
2023-06-19 19:29:58 -0700

    LL

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,15 +6,18 @@
  *
  *Return: 0
 */
+
+
 int main(void)
 {
 	int a;
 
+
 	a = '0';
 	while (a <= '9')
 	{
-        	putchar(a);
-        	if (a != '9')
+		putchar(a);
+		if (a != '9')
 		{
 			putchar(',');
 			putchar(' ');
