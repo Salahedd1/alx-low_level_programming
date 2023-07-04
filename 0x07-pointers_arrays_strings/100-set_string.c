80b1f7c289542b006ee12706d7c3d173d648bd54
salah <salahedinelassri@gmail.com>
2023-07-04 12:43:48 -0700

    THE LAST

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *set_string - strcpy advanced maybe
+ *@s: double pointer m that new
+ *@to: what to write in s
+ *Return: NADA
+*/
+
+void	set_string(char **s, char *to)
+{
+	*s = to;
+}
