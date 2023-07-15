b143319140b8d7a07ea3e5ef4d84264dde3827dd
salah <salahedinelassri@gmail.com>
2023-07-14 19:10:11 -0700

    MM

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,93 +1,101 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-void	_puts(char *str);
+#define ERR_MSG "Error"
 
 /**
- *opti_atoi - in this version it return error if ti find an alpha
- *@str: the str to change
- *Return: an int
-*/
-
-int	opti_atoi(char *str)
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
 {
-	int i;
-	unsigned long int result;
-	int signe;
+	int i = 0;
 
-	i = 0;
-	result = 0;
-	signe = 1;
-	while ((str[i] == '-') && str[i])
+	while (s[i])
 	{
-		if (str[i] == '-')
-			signe *= -1;
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
 		i++;
 	}
-	while (str[i] && str[i] >= '0' && str[i] <= '9')
-	{
-		result = (result * 10) + str[i] - '0';
-		i++;
-	}
-	return (result * signe);
+	return (1);
 }
 
 /**
- *printf_r - something to print , maybe printf isn;t allowed
- *@num: num to print
- *Return: NADA
-*/
-void	printf_r(unsigned long int num)
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
 {
-	unsigned long int div = 1;
-	int pt;
+	int i = 0;
 
-	while (num / div > 9)
-		div *= 10;
-	while (div >= 1)
+	while (s[i] != '\0')
 	{
-		pt = num / div;
-		_putchar('0' + pt);
-		num %= div;
-		div /= 10;
+		i++;
 	}
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
 	_putchar('\n');
-}
-
-/**
- *_puts - print str
- *@str: the str
- *Return: no return
-*/
-
-void _puts(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
-}
-
-/**
- *main - programe that giv the multiple of 2 num
- *@ac: number of argument
- *@av: arguments
- *Return: 0 or 98 in error
-*/
-
-int main(int ac, char **av)
-{
-	if (ac == 3)
-	{
-		printf_r(opti_atoi(av[1]) * opti_atoi(av[2]));
-	}
-	else
-	{
-		_puts("Error\n");
-		exit(98);
-	}
+	free(result);
 	return (0);
 }
