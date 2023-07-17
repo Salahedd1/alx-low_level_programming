afa677fa37b5cabb14153073acd23e10c18ef236
salah <salahedinelassri@gmail.com>
2023-07-17 11:30:25 -0700

    LL

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - statring point , print name of the file
+ *Return: 0 always
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
